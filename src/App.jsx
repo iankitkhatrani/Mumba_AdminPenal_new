bde41b72b6b312c86ea7446baf1de9936d708ad7
ankit <iankitkhatrani@gmail.com>
2024-02-06 15:07:43 +0530

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
     console.log("URL ",url)
 
     if(url[1] != "/signin"){
-      window.location.href = 'http://51.21.131.138:5175/signin'; //"http://192.168.0.203:5175/signin" //
+      window.location.href ='http://51.21.131.138:5175/signin'; // "http://192.168.0.203:5175/signin" //
       return false
     }
     //return (<Router><Login/></Router>)
